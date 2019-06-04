899758016c0ba31678794b75df0a335313aaf927
Patrick Queres <patrickqueres@gmail.com>
2019-06-04 19:39:58 -0300

    Add files via upload

---
+++ crawler_Correios.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+
+Instituto de Ciência e Tecnologia (ICT/UFF)
+Aluno: Patrick Souza Queres
+Data: 06/06/2019
+
+Created on Wed May 29 23:21:26 2019
+@author: patrickqueres
+
+"""
+
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+# objeto de dados com o código a ser rastreado
+data = {
+    'objetos': 'LS873105012CH',
+}
+
+
+# Realiza um POST e download da página através da biblioteca request
+url = "https://www2.correios.com.br/sistemas/rastreamento/resultado.cfm"
+response = requests.post(url, data=data)
+
+
+# Uso do BeautifulSoup para extrair a tabela de class/id "listing" e armazena no objeto table
+doc = BeautifulSoup(response.text, 'html.parser')
+correios = doc.find("table", {"class":"listEvent sro"} )
+
+
+# Converte a tabela em um dataframe utilizando a biblioteca Pandas
+table_str = str(correios)
+df = pd.read_html(table_str, header=0, index_col=0)[0]
+
+
+print (df.head(10))
+
+
+local_do_csv = 'correios.csv'
+df.to_csv (local_do_csv, index=True, header=True, encoding="utf-8")
--- crawler_SELIC.py
+++ crawler_SELIC.py
@@ -1,53 +1,50 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Apr 24 22:13:50 2019
+Instituto de Ciência e Tecnologia (ICT/UFF)
+Aluno: Patrick Souza Queres
+Data: 06/06/2019
 
+Created on Wed Apr 24 22:13:50 2019
 @author: patrickqueres
+
+
 """
+import requests 
 import pandas as pd
-import requests
 import numpy as np
 from bs4 import BeautifulSoup
 
+# Realiza o download da página através da biblioteca request
 req = requests.get('http://receita.economia.gov.br/orientacao/tributaria/pagamentos-e-parcelamentos/taxa-de-juros-selic')
 if req.status_code == 200:
     content = req.content
-    
-# Extrai a primeira tabela de alíquotas do site
+
+
+# Uso do BeautifulSoup para extrair a tabela de class/id "listing" e armazena no objeto table
 soup = BeautifulSoup(content, 'html.parser')
 table = soup.find("table", {"class":"listing"} )
 
-# Converte a tabela em um dataframe
+
+# Converte a tabela em um dataframe utilizando a biblioteca Pandas
 table_str = str(table)
 df = pd.read_html(table_str, header=0, index_col=0)[0]
 
-#Substitui os valores NaN por 0
+
+#Substitui os valores "NaN" por 0
 df = df.replace(np.nan, '0,00')
-# df.fillna(0) -- não funcionou...
 
 
-# Remove o simbolo de % dos valores -- MUITA GAMBIARRA!
+# Remove o simbolo de % dos valores
 meus_headers = list(df)
-#for header_atual in meus_headers:
-    # print (header_atual)
-    # df[header_atual] = df[header_atual].map(lambda x: x.rstrip('%'))
-# df['2011'] = df['2011'].map(lambda x: x.rstrip('%'))
-df['2011'] = df['2011'].map(lambda x: x.rstrip('%'))
-df['2011'] = df['2011'].map(lambda x: x.rstrip('%'))
-df['2012'] = df['2012'].map(lambda x: x.rstrip('%'))
-df['2013'] = df['2013'].map(lambda x: x.rstrip('%'))
-df['2014'] = df['2014'].map(lambda x: x.rstrip('%'))
-df['2015'] = df['2015'].map(lambda x: x.rstrip('%'))
-df['2016'] = df['2016'].map(lambda x: x.rstrip('%'))
-df['2017'] = df['2017'].map(lambda x: x.rstrip('%'))
-df['2018'] = df['2018'].map(lambda x: x.rstrip('%'))
-df['2019'] = df['2019'].map(lambda x: x.rstrip('%'))
+for header_atual in meus_headers:
+     df[header_atual] = df[header_atual].map(lambda x: x.rstrip('%'))
 
 
+# imprime os 12 primeiros itens do dataframe
 print (df.head(12))
 
-# local_do_csv = 'p:/Documents/Treinamentos e aperfeiçoamentos/Mestrado/2019_01/Algoritmos e Estruturas de Dados/projeto/teste.csv'
+# Converte o dataframe em arquivo CSV
 local_do_csv = 'selic.csv'
-df.to_csv (local_do_csv, index = True, header=True, encoding="utf-8")
+df.to_csv (local_do_csv, index=True, header=True, encoding="utf-8", sep=";")
 
 print('O arquivo selic.csv foi salvo na pasta do projeto!')
